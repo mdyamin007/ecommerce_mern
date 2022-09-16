ddc0fcb03ebbcac87681765632d76aa99a59221f
Md Yamin <mdyamin007@yahoo.com>
2022-09-16 21:53:31 +0600

    logo added

--- client/src/common/NavBar/index.jsx
+++ client/src/common/NavBar/index.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
+import Logo from "../../assets/images/logo.jpg";
+
 const NavBar = () => {
   return (
-    <nav className="w-full flex justify-between items-center px-20 py-6 shadow">
-      <div>
-        <Link to="/">
+    <nav className="w-full flex justify-between items-center px-20 py-3 shadow">
+      <Link to="/">
+        <div className="flex justify-center items-center">
+          <img src={Logo} className="h-12 w-12 mx-5" />
           <h1 className="text-4xl font-semibold text-teal-500 font-serif">
-            Ashraful's Online Market
+            Safe Market
           </h1>
-        </Link>
-      </div>
+        </div>
+      </Link>
       <nav>
         <Link to="/seller-checker">
           <div className="text-teal-500">Check Seller</div>
